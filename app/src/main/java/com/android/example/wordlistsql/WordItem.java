cf634df7dd59c281ad59b6cc678484704c467cef
Bryon Nicoson <bryon.nicoson@gmail.com>
2018-03-10 19:39:53 -0600

    Initial commit

---
+++ app/src/main/java/com/android/example/wordlistsql/WordItem.java
@@ -0,0 +1,29 @@
+package com.android.example.wordlistsql;
+
+/**
+ * Created by bryon on 3/10/18.
+ */
+
+public class WordItem {
+
+    private int mId;
+    private String mWord;
+
+    public WordItem(){}
+
+    public int getmId() {
+        return mId;
+    }
+
+    public void setmId(int mId) {
+        this.mId = mId;
+    }
+
+    public String getmWord() {
+        return mWord;
+    }
+
+    public void setmWord(String mWord) {
+        this.mWord = mWord;
+    }
+}
